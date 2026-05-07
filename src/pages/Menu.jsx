ce5761fcfc46326871fe14bd2e99c3a5ffc5d208
Navin8491 <navin8491@gmail.com>
2026-05-07 20:25:05 +0530

    Remove IntroSection and MenuPreview from Menu page

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import PageHeader from '../components/PageHeader';
-import IntroSection from '../components/IntroSection';
-import MenuPreview from '../components/MenuPreview';
 import ProductsTab from '../components/ProductsTab';
 
 const Menu = () => {
   return (
     <div>
       <PageHeader title="Our Menu" bgImage="/images/bg_3.jpg" />
-      <IntroSection />
-      <MenuPreview />
       <ProductsTab />
     </div>
   );
